27704062528e1cbae93e1f71ecf31c3ddeca4a7b
James Walsh <jamesandersonwalsh@gmail.com>
2019-02-01 15:47:34 -0700

    add graphql schema for empire

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import { ApolloServer, gql } from 'apollo-server'
 
-// This is a (sample) collection of books we'll be able to query
+// This is a (sample) collection of team members we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
 // from an existing data source like a REST API or database.
-const books = [
+const fakeEmpireDatabaseData = [
   {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
+    firstName: 'James',
+    lastName: 'Walsh',
+    role: "Developer",
+    catchPhrase: "What a time to be alive...",
+    estimatedTimeTillNextBurpInMinutes: 25,
+    isAwesome: true
   },
   {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
+    firstName: 'Kaiden',
+    lastName: 'Rawlinson',
+    role: "Tech Lead",
+    catchPhrase: `I said treat yo'self, not let yo'self go!`,
+    estimatedTimeTillNextBurpInMinutes: 999999,
+    isAwesome: true
   },
+  {
+    firstName: 'Bressain',
+    lastName: 'Dinkelman',
+    role: "Former Tech Lead",
+    catchPhrase: "The lesson is... never try.",
+    estimatedTimeTillNextBurpInMinutes: 5,
+    isAwesome: true
+  },
+  {
+    firstName: 'Zach',
+    lastName: 'Walton',
+    role: "Developer",
+    catchPhrase: "I'll make my own destiny. Believe it!",
+    estimatedTimeTillNextBurpInMinutes: null,
+    isAwesome: true
+  },
+  {
+    firstName: 'Isaac',
+    lastName: 'Stennett',
+    role: "Developer",
+    catchPhrase: null,
+    estimatedTimeTillNextBurpInMinutes: null,
+    isAwesome: true
+  },
+  {
+    firstName: 'Cole',
+    lastName: 'Quartuccio',
+    role: "Designer",
+    catchPhrase: "Does that fit within an 8 pixel grid?",
+    estimatedTimeTillNextBurpInMinutes: null,
+    isAwesome: true
+  },
+  {
+    firstName: 'Dave',
+    lastName: 'Rackham',
+    role: "Product Manager",
+    catchPhrase: null,
+    estimatedTimeTillNextBurpInMinutes: null,
+    isAwesome: true
+  }
 ]
 
 // Type definitions define the "shape" of your data and specify
@@ -19,24 +67,32 @@ const books = [
 const typeDefs = gql`
   # Comments in GraphQL are defined with the
 
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
+  # This "DevStarMember" type can be used in other type declarations.
+  type DevStarMember {
+    firstName: String!
+    lastName: String!
+    role: String!
+    catchPhrase: String
+    estimatedTimeTillNextBurpInMinutes: Int
+    isAwesome: Boolean
   }
 
   # The "Query" type is the root of all GraphQL queries.
   # (A "Mutation" type will be covered later on.)
   type Query {
-    books: [Book]
+    empire: [DevStarMember]
   }
 `
+
+function getEmpireCommand() {
+  return fakeEmpireDatabaseData
+}
 
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    empire: getEmpireCommand,
   }
 }
 
